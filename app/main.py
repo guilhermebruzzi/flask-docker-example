ba84a86d22a53fe24f07d946ca29532c674ad55a
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-06-28 01:41:21 -0300

    Render html and json

--- app/main.py
+++ app/main.py
@@ -1,13 +1,49 @@
 import os
+from datetime import datetime
 
-from flask import Flask
+from flask import Flask, jsonify, render_template_string
 
 app = Flask(__name__)
+
+# def render_template(path, **data):
+#     if not "debug" in data.keys():
+#         data["debug"] = app.config["DEBUG"]
+#
+#     return flask.render_template(path, **data)
 
 
 @app.route("/")
 def hello():
-    return "Hello World from Flask! :D"
+    return jsonify({
+        "name": "Hello World from Flask! 2",
+        "count": 0,
+        "created": datetime.utcnow(),
+    })
+
+
+@app.route("/index.html")
+def hello_html():
+    template = """
+        <table>
+            <tr>
+                <th>name</th>
+                <th>count</th>
+                <th>created</th>
+            </tr>
+            <tr>
+                <td>{{name}}</td>
+                <td>{{count}}</td>
+                <td>{{created}}</td>
+            </tr>
+        </table>
+    """
+
+    return render_template_string(
+        template,
+        name="Hello World from Flask! 2",
+        count=0,
+        created=datetime.utcnow()
+    )
 
 
 if __name__ == "__main__":
